6c3f1c3c8b2afc61084a7927560d10237abda316
Nafis Ahmed Utsho <nafisahmedutsho13@gmail.com>
2024-12-31 00:37:10 +0600

    add files

---
+++ EaseLife/routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Http\Livewire\Admin\AdminDashboardComponent;
+use App\Http\Livewire\Admin\AdminServiceCategoryComponent;
+use App\Http\Livewire\Customer\CustomerDashboardComponent;
+use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ServiceCategoriesComponent;
+use App\Http\Livewire\Sprovider\SproviderDashboardComponent;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/',HomeComponent::class)->name('home');
+Route::get('/service-categories',ServiceCategoriesComponent::class)->name('home.service_categories');
+
+Route::middleware(['auth:sanctum','verified'])->group(function(){
+    Route::get('/customer/dashboard',CustomerDashboardComponent::class)->name('customer.dashboard');
+});
+
+
+
+Route::middleware(['auth:sanctum','verified','authsprovider'])->group(function(){
+    Route::get('/sprovider/dashboard',SproviderDashboardComponent::class)->name('sprovider.dashboard');
+});
+
+Route::middleware(['auth:sanctum','verified','authadmin'])->group(function(){
+    Route::get('/admin/dashboard',AdminDashboardComponent::class)->name('admin.dashboard');
+    Route::get('/admin/service-categories',AdminServiceCategoryComponent::class)->name('admin.service_categories');
+});
